9250f819c4e55174e4fdee6fd657824494cdf209
Ian Johnson <ijohnson@covermymeds.com>
2019-05-30 11:16:21 -0400

    Add a form

--- lib/gallows_web/controllers/hangman_controller.ex
+++ lib/gallows_web/controllers/hangman_controller.ex
@@ -12,4 +12,15 @@ defmodule GallowsWeb.HangmanController do
     |> put_session(:game, game)
     |> render("game_field.html", tally: tally)
   end
+
+  def make_move(conn, params) do
+    # raise inspect params
+    guess = params["make_move"]["guess"]
+    tally =
+      conn
+      |> get_session(:game)
+      |> Hangman.make_move(guess)
+    put_in(conn.params["make_move"]["guess"], "")
+    |> render("game_field.html", tally: tally)
+  end
 end
