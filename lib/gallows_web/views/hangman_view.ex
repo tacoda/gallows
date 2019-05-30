8f94e00563fcfd1cc74981fa1d6eddfc9f0c5eff
Ian Johnson <ijohnson@covermymeds.com>
2019-05-30 09:56:53 -0400

    Create the initial server

---
+++ lib/gallows_web/views/hangman_view.ex
@@ -0,0 +1,7 @@
+defmodule GallowsWeb.HangmanView do
+  use GallowsWeb, :view
+
+  def word_so_far(tally) do
+    tally.letters |> Enum.join(" ")
+  end
+end
--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Gallows.MixProject do
   def application do
     [
       mod: {Gallows.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools],
     ]
   end
 
@@ -38,7 +38,9 @@ defmodule Gallows.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+
+      {:hangman, [ path: "../hangman" ]},
     ]
   end
 end
