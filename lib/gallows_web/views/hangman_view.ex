9d5bd61879c3a0c00ec314459b378207095abcd6
Ian Johnson <ijohnson@covermymeds.com>
2019-06-03 13:52:29 -0400

    Add more helpers

--- lib/gallows_web/views/hangman_view.ex
+++ lib/gallows_web/views/hangman_view.ex
@@ -1,7 +1,18 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
 
+  import GallowsWeb.Views.Helpers.GameStateHelper
+
+  def new_game_button(conn) do
+    button("New Game", to: Routes.hangman_path(conn, :create_game))
+  end
+
   def word_so_far(tally) do
     tally.letters |> Enum.join(" ")
   end
+
+  def game_over?(%{ game_state: game_state }) do
+    game_state in [ :won, :lost ]
+  end
+
 end
