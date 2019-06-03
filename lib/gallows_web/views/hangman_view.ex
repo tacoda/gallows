40e1f268af601d55fedd68813b1514c161bbc089
Ian Johnson <ijohnson@covermymeds.com>
2019-06-03 14:20:54 -0400

    Add graphics

--- lib/gallows_web/views/hangman_view.ex
+++ lib/gallows_web/views/hangman_view.ex
@@ -15,4 +15,12 @@ defmodule GallowsWeb.HangmanView do
     game_state in [ :won, :lost ]
   end
 
+  def turn(left, target) when target >= left do
+    "opacity: 1"
+  end
+
+  def turn(left, target) do
+    "opacity: 0.1"
+  end
+
 end
